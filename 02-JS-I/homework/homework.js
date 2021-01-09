3192931c1e56c9c5bb71b44356984b6ca7ac44ea
crismurbae <crismurbae@gmail.com>
2021-01-09 10:17:13 -0300

    resolución del homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -178,12 +178,14 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  
   return "Hola "+nombre+"!";
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+
   return alto*ancho;
 }
 
