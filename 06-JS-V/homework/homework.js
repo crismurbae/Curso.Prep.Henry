66ec54da34145df197269eb7db9c6d14b3fec67a
crismurbae <crismurbae@gmail.com>
2021-01-17 17:46:56 -0300

    resolución homework

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -4,18 +4,37 @@ function crearUsuario() {
   // Crea una Clase de ES6 o una función constructor llamada "Usuario"
   // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
   // En el `contructor`, define el usuario, el nombre, el email y la contraseña
-  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
+  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 
+  // 'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
 
+  class Usuario {
+
+        constructor (opciones) {
+          this.usuario = opciones.usuario;
+          this.nombre = opciones.nombre;
+          this.email = opciones.email;
+          this.password = opciones.password;
+        }
+
+        saludar() {
+          return 'Hola, mi nombre es ' + this.nombre;
+        }
+
+ }
+ return Usuario
 }
+
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
-
+    Constructor.prototype.saludar = function() {
+      return 'Hello World!';
+    }
 }
 
 function agregarStringInvertida() {
@@ -24,7 +43,13 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-
+  String.prototype.reverse = function() {
+    var alreves = ''
+    for (var i = this.length - 1; i >= 0; i--) {
+      alreves = alreves + this[i] 
+    }
+    return alreves;
+  }
 }
 
 // No modificar nada debajo de esta línea
@@ -34,4 +59,4 @@ module.exports = {
   crearUsuario,
   agregarMetodoPrototype,
   agregarStringInvertida
-};
+}
